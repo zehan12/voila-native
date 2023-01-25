9a1da40a28a548b11c2b8212d326de4e3368b9bc
Zehan Khan <zehan9211@gmail.com>
2023-01-25 20:44:31 +0530

    install expo cli and init expo app

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading} >React in Mobile</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'hotpink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border:"2px solid black"
+  },
+  heading:{
+    fontSize:"20px",
+    color:"blue",
+    border:"2px solid black"
+  }
+});
